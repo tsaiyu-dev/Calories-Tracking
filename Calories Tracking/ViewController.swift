67db52b1b26b43f4f8440103f1ea6e62cb62672f
Amy Hung <amyhung0123@gmail.com>
2023-04-04 17:22:25 +0800

    Initial Commit

---
+++ Calories Tracking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calories Tracking
+//
+//  Created by Amy_H on 4/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
